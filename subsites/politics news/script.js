336a159674b86a17d4515c7b039727eda237ffe0
ShehabMkk <shehab.mkk2004@gmail.com>
2023-12-14 01:28:18 +0200

    1

---
+++ subsites/politics news/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var slideIndex = 0;
+  var slides = $('.slide');
+  var totalSlides = slides.length;
+  var slideInterval = setInterval(nextSlide, 1500);
+  var bulletArray = [];
+
+  function nextSlide() {
+    slides.eq(slideIndex).fadeOut();
+    slideIndex = (slideIndex + 1) % totalSlides;
+    slides.eq(slideIndex).fadeIn();
+    updateBullet(slideIndex);
+  }
+
+  function updateBullet(index) {
+    $('.slide_buttons .slide_btn').removeClass('active');
+    bulletArray[index].addClass('active');
+  }
+
+  slides.each(function(index) {
+    var $button = $('<a class="slide_btn">&bull;</a>');
+    if (index === slideIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function() {
+      clearInterval(slideInterval);
+      slides.eq(slideIndex).fadeOut();
+      slideIndex = index;
+      slides.eq(slideIndex).fadeIn();
+      updateBullet(slideIndex);
+      slideInterval = setInterval(nextSlide, 3000);
+    }).appendTo('.slide_buttons');
+    bulletArray.push($button);
+  });
+});
