337b331a922c4bbfad66a56f779d719b25b7899c
omarovici <omarkhalidsaber042004@gmail.com>
2023-12-14 00:11:04 +0200

    1

--- script.js
+++ script.js
@@ -1,89 +1,35 @@
-$('.slider').each(function() {
-  var $this = $(this);
-  var $group = $this.find('.slide_group');
-  var $slides = $this.find('.slide');
+$(document).ready(function() {
+  var slideIndex = 0;
+  var slides = $('.slide');
+  var totalSlides = slides.length;
+  var slideInterval = setInterval(nextSlide, 1500);
   var bulletArray = [];
-  var currentIndex = 0;
-  var timeout;
-  
-  function move(newIndex) {
-    var animateLeft, slideLeft;
-    
-    advance();
-    
-    if ($group.is(':animated') || currentIndex === newIndex) {
-      return;
-    }
-    
-    bulletArray[currentIndex].removeClass('active');
-    bulletArray[newIndex].addClass('active');
-    
-    if (newIndex > currentIndex) {
-      slideLeft = '100%';
-      animateLeft = '-100%';
-    } else {
-      slideLeft = '-100%';
-      animateLeft = '100%';
-    }
-    
-    $slides.eq(newIndex).css({
-      display: 'block',
-      left: slideLeft
-    });
-    $group.animate({
-      left: animateLeft
-    }, function() {
-      $slides.eq(currentIndex).css({
-        display: 'none'
-      });
-      $slides.eq(newIndex).css({
-        left: 0
-      });
-      $group.css({
-        left: 0
-      });
-      currentIndex = newIndex;
-    });
+
+  function nextSlide() {
+    slides.eq(slideIndex).fadeOut();
+    slideIndex = (slideIndex + 1) % totalSlides;
+    slides.eq(slideIndex).fadeIn();
+    updateBullet(slideIndex);
   }
-  
-  function advance() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 4000);
+
+  function updateBullet(index) {
+    $('.slide_buttons .slide_btn').removeClass('active');
+    bulletArray[index].addClass('active');
   }
-  
-  $('.next_btn').on('click', function() {
-    if (currentIndex < ($slides.length - 1)) {
-      move(currentIndex + 1);
-    } else {
-      move(0);
-    }
-  });
-  
-  $('.previous_btn').on('click', function() {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
-  });
-  
-  $.each($slides, function(index) {
+
+  slides.each(function(index) {
     var $button = $('<a class="slide_btn">&bull;</a>');
-    
-    if (index === currentIndex) {
+    if (index === slideIndex) {
       $button.addClass('active');
     }
     $button.on('click', function() {
-      move(index);
+      clearInterval(slideInterval);
+      slides.eq(slideIndex).fadeOut();
+      slideIndex = index;
+      slides.eq(slideIndex).fadeIn();
+      updateBullet(slideIndex);
+      slideInterval = setInterval(nextSlide, 3000);
     }).appendTo('.slide_buttons');
     bulletArray.push($button);
   });
-  
-  advance();
 });
